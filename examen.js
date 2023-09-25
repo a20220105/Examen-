8ba6f5b13b6643af6615962aa38fd8c1ff3e2699
a20220105 <131031514+a20220105@users.noreply.github.com>
2023-09-25 13:27:28 -0500

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,26 @@
+nombre = prompt("Inserte su nombre:")
+alert=("Hola" + nombre + "Bienvenid@ al cuestionario de los mares")
+var correcta = 0
+var incorrecta = 0 
+
+
+var respuesta1 = prompt(" 1. ¿Cuánta agua hay en la Tierra?\nA. Alrededor de un 41% de la superficie terrestre es agua.\nB. Alrededor de un 51% de la superficie terrestre es agua\nC. Alrededor de un 71% de la superficie terrestre es agua");
+
+var respuesta3 = prompt("2.¿Qué océano es el más grande?\nA. El Pacífico\nB. El Atlántico\nC. El Índico");
+
+var respuesta4 = prompt("3. ¿Cuánto mide la parte más profunda de los Océanos?\nA. 11 Km\nB. 23 Km\nC. 31 Km");
+
+if (respuesta == "C"){ 
+}
+
+if (respuesta2 == "A"){
+}
+
+if(respuesta3 == "A"){
+}
+
+if(puntaje <= 50){
+    document.write(nombre+ " Lo has hecho bien " + puntaje + "FELICITACIONES");
+}else{
+    document.write(nombre + " Puedes haber hecho mejor " + puntaje + "VUELVE A INTENTARLO");
+}
